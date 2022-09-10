ea639f0102f98ebe0a34fcb42fd46e3eb17ff309
Baohui Jia <bjia36@gatech.edu>
2022-09-09 18:17:17 -0700

    get post replace binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,24 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatExpansionModule } from '@angular/material/expansion'
 
 
 import { AppComponent } from './app.component';
-import { PostCreatecomponent } from './posts/post-create/post-create.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { HeaderComponent } from './header/header.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostCreatecomponent,
-    HeaderComponent
+    PostCreateComponent,
+    HeaderComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule,
@@ -28,7 +30,8 @@ import { HeaderComponent } from './header/header.component';
     MatInputModule,
     MatCardModule,
     MatButtonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
